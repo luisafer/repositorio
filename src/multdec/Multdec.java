194fc9a8f7996378389da630524b248767ff1308
ACER <ACER@Luisa>
2014-10-20 14:35:53 -0500

    multiplicacacion y division decimal Math

---
+++ src/multdec/Multdec.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multdec;
+
+import java.util.Scanner;
+
+public class Multdec {
+
+/**
+* @param args
+*/
+public static void main(String[] args) {
+
+Scanner entrada = new Scanner(System.in);
+double num, num2, result, resultD, decim=10, resultfinal, resultfinalD;
+
+System.out.println( "Introduce su numero:" );
+num = entrada.nextDouble();
+System.out.println( "Introduce segundo numero :" );
+num2 = entrada.nextDouble();
+
+result=	num*num2;
+resultD=num/num2;
+resultfinal= Math.round(result*Math.pow(10, decim)/Math.pow(10, decim));
+resultfinalD= Math.round(resultD*Math.pow(10, decim)/Math.pow(10, decim));
+
+System.out.print( "el final de la operacion multiplicada es " + resultfinal +" y dividida es "+resultfinalD);
+}
+}
